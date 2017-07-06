15a8aeda9e2ca34cef6ca23d7e39ec19f0bd6c7d
erikzaxaryan <zaxaryan11@gmail.com>
2017-07-06 15:57:01 +0400

    Create Vlg.js

---
+++ Vlg.js
@@ -0,0 +1,10 @@
+function priceCheck(price) {
+  if (price > 1000) {
+    return "That's expensive!"
+  }
+  if (price < 100) {
+    return "That's cheap!";
+  }
+  if (price = 0) {
+  return "Not bad...";
+}
